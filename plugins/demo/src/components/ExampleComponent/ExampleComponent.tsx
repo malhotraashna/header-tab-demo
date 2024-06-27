1b33a79082cd699f264624f273a75df5a7a2154f
Ashna Malhotra <ashna.malhotra@booking.com>
2024-06-27 22:06:19 +0200

    reproduce HeaderTabs issue

--- plugins/demo/src/components/ExampleComponent/ExampleComponent.tsx
+++ plugins/demo/src/components/ExampleComponent/ExampleComponent.tsx
@@ -1,38 +1,77 @@
 import React from 'react';
-import { Typography, Grid } from '@material-ui/core';
-import {
-  InfoCard,
-  Header,
-  Page,
-  Content,
-  ContentHeader,
-  HeaderLabel,
-  SupportButton,
-} from '@backstage/core-components';
-import { ExampleFetchComponent } from '../ExampleFetchComponent';
+import { Grid } from '@material-ui/core';
+import { Content, HeaderLabel, HeaderTabs, Page, Header } from '@backstage/core-components';
+import { Route, Routes } from 'react-router';
 
-export const ExampleComponent = () => (
+const tabs = [
+  { label: 'Services', path: '' },
+  { label: 'Runs', path: 'runs' },
+  { label: 'Scenarios', path: 'scenarios' },
+];
+
+const PluginHeader = () => (
+  <Header title="Welcome to demo!" subtitle="Optional subtitle">
+    <HeaderLabel label="Owner" value="Team X" />
+  </Header>
+);
+
+const PageTabs = (props: { selectedIndex: number }) => {
+  return (
+    <HeaderTabs
+      {...props}
+      tabs={tabs.map(({ label, path }) => ({
+        id: path,
+        label,
+      }))}
+    />
+  );
+};
+
+export const ServicesPage = () => (
   <Page themeId="tool">
-    <Header title="Welcome to demo!" subtitle="Optional subtitle">
-      <HeaderLabel label="Owner" value="Team X" />
-      <HeaderLabel label="Lifecycle" value="Alpha" />
-    </Header>
+    <PluginHeader />
+    <PageTabs selectedIndex={0} />
     <Content>
-      <ContentHeader title="Plugin title">
-        <SupportButton>A description of your plugin goes here.</SupportButton>
-      </ContentHeader>
       <Grid container spacing={3} direction="column">
         <Grid item>
-          <InfoCard title="Information card">
-            <Typography variant="body1">
-              All content should be wrapped in a card like this.
-            </Typography>
-          </InfoCard>
-        </Grid>
-        <Grid item>
-          <ExampleFetchComponent />
+          <div>Services Page</div>
         </Grid>
       </Grid>
     </Content>
   </Page>
+);
+
+export const ScenarioPage = () => (
+  <Page themeId="tool">
+    <PluginHeader />
+    <PageTabs selectedIndex={2} />
+    <Content>
+      <Grid container spacing={3} direction="column">
+        <Grid item>
+          <div>Scenario Page</div>
+        </Grid>
+      </Grid>
+    </Content>
+  </Page>
+);
+
+export const ResultsPage = () => (
+  <Page themeId="tool">
+    <PluginHeader />
+    <Content>
+      <Grid container spacing={3} direction="column">
+        <Grid item>
+          <div>Results Page</div>
+        </Grid>
+      </Grid>
+    </Content>
+  </Page>
+);
+
+export const ExampleComponent = () => (
+  <Routes>
+    <Route path="/" element={<ServicesPage />} />
+    <Route path="/scenarios" element={<ScenarioPage />} />
+    <Route path="/results" element={<ResultsPage />} />
+  </Routes>
 );
